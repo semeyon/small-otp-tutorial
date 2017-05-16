2627a30f871d053abfdc8c5eb768ca3912a19e35
smartpeople <semeyon@speople.pro>
2017-05-16 18:26:01 +0600

    Timeout support

--- lib/otp/echo.ex
+++ lib/otp/echo.ex
@@ -1,4 +1,5 @@
 defmodule OTP.Echo do
+
     def start_link do
         pid = spawn_link(__MODULE__, :loop, [])
         {:ok, pid}
@@ -8,11 +9,30 @@ defmodule OTP.Echo do
         Kernel.send(pid, {msg, self()})
     end
 
+    def async_send(pid, msg) do
+        Kernel.send(pid, {msg, self()})
+    end
+
+    # @loop_timeout 10
+    # @sync_send_timeout 200
+
+    def sync_send(pid, msg) do
+        async_send(pid, msg)
+        receive do
+            msg -> msg
+        end
+    end
+
     def loop do
         receive do
-            {msg, caller} when is_pid(caller) ->
+            {:no_replay, _caller} -> 
+                loop()
+            {msg, caller} ->
                 Kernel.send(caller, msg)
                 loop()
+        after
+            10 ->
+                exit(:normal)
         end
     end
 
--- test/otp/echo_test.exs
+++ test/otp/echo_test.exs
@@ -1,13 +1,38 @@
 defmodule OTP.EchoTest do
     use ExUnit.Case
-
+    import Logger
     alias OTP.Echo
 
-    test "echo" do
+    # test "echo" do
+    #     {:ok, pid} = Echo.start_link()
+    #     Echo.send(pid, :hello)
+    #     assert_receive :hello
+    #     Echo.send(pid, :hi)
+    #     assert_receive :hi
+    # end
+
+    test "timeout" do
         {:ok, pid} = Echo.start_link()
-        Echo.send(pid, :hello)
+        Process.sleep(50)
+        assert false == Process.alive?(pid)
+    end
+
+    test "async echo" do
+        {:ok, pid} = Echo.start_link()
+        Echo.async_send(pid, :hello)
         assert_receive :hello
-        Echo.send(pid, :hi)
+        Echo.async_send(pid, :hi)
         assert_receive :hi
     end
+
+    test "sync echo" do
+        {:ok, pid} = Echo.start_link()
+        assert :hello == Echo.sync_send(pid, :hello)
+    end
+
+    # test "sync send timout" do
+    #     {:ok, pid} = Echo.start_link()
+    #     assert {:error, :timeout} == Echo.sync_send(pid, :no_reply)
+    # end
+
 end
